f31f41b42ef89943cf3bb4191de8f9bc743d50d3
Matthew Dennerlein <matthew@technecure.com>
2019-07-22 20:59:26 -0400

    Adding Logging System

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,7 +3,9 @@
 // For use by Hazel Applications
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ---Entry Point ------------------------------
 #include "Hazel/EntryPoint.h"
 // ---------------------------------------------
+
